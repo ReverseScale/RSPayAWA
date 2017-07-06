675e1bcecde8ec3839ed457d2fa45cf60dc6a5d2
XieTian <Stevens@MacBook-Air.local>
2017-07-06 17:48:59 +0800

    Update UI, Icon

---
+++ RSPayAWA/TableViewController.m
@@ -0,0 +1,269 @@
+//
+//  TableViewController.m
+//  RSPayAWA
+//
+//  Created by WhatsXie on 2017/7/6.
+//  Copyright © 2017年 StevenXie. All rights reserved.
+//
+
+#import "TableViewController.h"
+#import "RSPayManager.h"
+#import "RSApplePay.h"
+
+#define IOS_VERSION_10 (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_9_x_Max)?(YES):(NO)
+
+
+@interface TableViewController ()<RSApplePayDelegate>
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"----->%d",IOS_VERSION_10);
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+- (void)actionAliPay {
+    NSString *orderMessage = @"app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.02%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22314VYGIAGG7ZOYY%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA&timestamp=2016-08-15%2012%3A12%3A15&version=1.0&sign=MsbylYkCzlfYLy9PeRwUUIg9nZPeN9SfXPNavUCroGKR5Kqvx0nEnd3eRmKxJuthNUx4ERCXe552EV9PfwexqW%2B1wbKOdYtDIb4%2B7PL3Pc94RZL0zKaWcaY3tSL89%2FuAVUsQuFqEJdhIukuKygrXucvejOUgTCfoUdwTi7z%2BZzQ%3D";
+    [RSPAYMANAGER rs_payWithOrderMessage:orderMessage callBack:^(RSErrCode errCode, NSString *errStr) {
+        NSLog(@"errCode = %zd,errStr = %@",errCode,errStr);
+    }];
+}
+- (void)actionWechatPay {
+    //    NSLog(@"%@",[self jumpToBizPay]);
+    PayReq* req = [[PayReq alloc] init];
+    //    req.partnerId           = [dict objectForKey:@"partnerid"];
+    //    req.prepayId            = [dict objectForKey:@"prepayid"];
+    //    req.nonceStr            = [dict objectForKey:@"noncestr"];
+    //    req.timeStamp           = stamp.intValue;
+    //    req.package             = [dict objectForKey:@"package"];
+    //    req.sign                = [dict objectForKey:@"sign"];
+    req.partnerId = @"10000100";
+    req.prepayId= @"1101000000140415649af9fc314aa427";
+    req.package = @"Sign=WXPay";
+    req.nonceStr= @"a462b76e7436e98e0ed6e13c64b4fd1c";
+    req.timeStamp= @"1397527777".intValue;
+    req.sign= @"582282D72DD2B03AD892830965F428CB16E7A256";
+    
+    [RSPAYMANAGER rs_payWithOrderMessage:req callBack:^(RSErrCode errCode, NSString *errStr) {
+        NSLog(@"errCode = %zd,errStr = %@",errCode,errStr);
+    }];
+}
+- (void)actionApplePay {
+    RSApplePay *pay=[[RSApplePay alloc]initWithPayReceiver:@"申冠华" amount:@"5.20" presentVC:self];
+    [pay setDelegate:self];
+}
+#pragma mark - RSApplePay delegate
+//支付成功
+- (void)RSApplePayDidSuccess{
+    NSLog(@"支付成功!");
+}
+//支付失败
+- (void)RSApplePayDidFailure{
+    NSLog(@"支付失败!");
+}
+
+- (NSString *)generateTradeNO {
+    static int kNumber = 15;
+    
+    NSString *sourceStr = @"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    NSMutableString *resultStr = [[NSMutableString alloc] init];
+    srand((unsigned)time(0));
+    for (int i = 0; i < kNumber; i++)
+    {
+        unsigned index = rand() % [sourceStr length];
+        NSString *oneStr = [sourceStr substringWithRange:NSMakeRange(index, 1)];
+        [resultStr appendString:oneStr];
+    }
+    return resultStr;
+}
+
+#pragma mark - WeiChat about
+- (NSString *)jumpToBizPay {
+    //============================================================
+    /**
+     *  @author Clarence
+     *
+     *  来自微信文档数据
+     */
+    //============================================================
+    NSString *urlString   = @"http://wxpay.weixin.qq.com/pub_v2/app/app_pay.php?plat=ios";
+    //解析服务端返回json数据
+    NSError *error;
+    //加载一个NSURL对象
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+    //将请求的url数据放到NSData对象中
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request];
+    //小于ios9系统，因为ios9之后去掉了NSURLConnection
+    if (IOS_VERSION_10==0) {
+        if ( response != nil) {
+            NSMutableDictionary *dict = NULL;
+            //IOS5自带解析类NSJSONSerialization从response中解析出数据放到字典中
+            dict = [NSJSONSerialization JSONObjectWithData:response options:NSJSONReadingMutableLeaves error:&error];
+            
+            NSLog(@"url:%@",urlString);
+            if(dict != nil){
+                NSMutableString *retcode = [dict objectForKey:@"retcode"];
+                if (retcode.intValue == 0){
+                    NSMutableString *stamp  = [dict objectForKey:@"timestamp"];
+                    
+                    //调起微信支付
+                    PayReq* req             = [[PayReq alloc] init];
+                    req.partnerId           = [dict objectForKey:@"partnerid"];
+                    req.prepayId            = [dict objectForKey:@"prepayid"];
+                    req.nonceStr            = [dict objectForKey:@"noncestr"];
+                    req.timeStamp           = stamp.intValue;
+                    req.package             = [dict objectForKey:@"package"];
+                    req.sign                = [dict objectForKey:@"sign"];
+                    
+                    
+                    [RSPAYMANAGER rs_payWithOrderMessage:req callBack:^(RSErrCode errCode, NSString *errStr) {
+                        NSLog(@"errCode = %zd,errStr = %@",errCode,errStr);
+                    }];
+                    
+                    //日志输出
+                    NSLog(@"appid=%@\npartid=%@\nprepayid=%@\nnoncestr=%@\ntimestamp=%ld\npackage=%@\nsign=%@",[dict objectForKey:@"appid"],req.partnerId,req.prepayId,req.nonceStr,(long)req.timeStamp,req.package,req.sign );
+                    return @"";
+                }else{
+                    return [dict objectForKey:@"retmsg"];
+                }
+            }else{
+                return @"服务器返回错误，未获取到json对象";
+            }
+        }else{
+            return @"服务器返回错误";
+        }
+    }else{
+        if ( task != nil) {
+            NSMutableDictionary *dict = NULL;
+            //IOS5自带解析类NSJSONSerialization从response中解析出数据放到字典中
+            dict = [NSJSONSerialization JSONObjectWithData:response options:NSJSONReadingMutableLeaves error:&error];
+            
+            NSLog(@"url:%@",urlString);
+            if(dict != nil){
+                NSMutableString *retcode = [dict objectForKey:@"retcode"];
+                if (retcode.intValue == 0){
+                    NSMutableString *stamp  = [dict objectForKey:@"timestamp"];
+                    
+                    //调起微信支付
+                    PayReq* req             = [[PayReq alloc] init];
+                    req.partnerId           = [dict objectForKey:@"partnerid"];
+                    req.prepayId            = [dict objectForKey:@"prepayid"];
+                    req.nonceStr            = [dict objectForKey:@"noncestr"];
+                    req.timeStamp           = stamp.intValue;
+                    req.package             = [dict objectForKey:@"package"];
+                    req.sign                = [dict objectForKey:@"sign"];
+                    
+                    
+                    [RSPAYMANAGER rs_payWithOrderMessage:req callBack:^(RSErrCode errCode, NSString *errStr) {
+                        NSLog(@"errCode = %zd,errStr = %@",errCode,errStr);
+                    }];
+                    
+                    //日志输出
+                    NSLog(@"appid=%@\npartid=%@\nprepayid=%@\nnoncestr=%@\ntimestamp=%ld\npackage=%@\nsign=%@",[dict objectForKey:@"appid"],req.partnerId,req.prepayId,req.nonceStr,(long)req.timeStamp,req.package,req.sign );
+                    return @"";
+                }else{
+                    return [dict objectForKey:@"retmsg"];
+                }
+            }else{
+                return @"服务器返回错误，未获取到json对象";
+            }
+        }else{
+            return @"服务器返回错误";
+        }
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row) {
+        case 0:
+            [self actionAliPay];
+            break;
+        case 1:
+            [self actionWechatPay];
+            break;
+        default:
+            [self actionApplePay];
+            break;
+    }
+}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//#warning Incomplete implementation, return the number of sections
+//    return 0;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//#warning Incomplete implementation, return the number of rows
+//    return 0;
+//}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
